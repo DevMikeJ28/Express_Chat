552c8ec57f580ba9b345de0ea50fd03dbee61989
SuKhoi <tungvsfpt@gmail.com>
2025-10-05 11:50:00 +0700

    Change base inti postgree

--- src/configs/databases/init.postgresdb.ts
+++ src/configs/databases/init.postgresdb.ts
@@ -1,13 +1,28 @@
 import pgPromise from "pg-promise";
 import monitor from "pg-monitor";
 
-const pgp = pgPromise();
-const connections: Record<string, string> = {
-  user:
-    process.env.POSTGRESQL_USER ||
-    "postgres://postgres:sa@localhost:5432/users",
-};
+const pgp = pgPromise({});
 
-export const createPostgresConnection = (databaseName: string) => {
+monitor.attach(pgp);
 
+export const newPostgresConnection = (uri: string) => {
+  const db = pgp(uri);
+
+  //Test Connection:
+  db.connect()
+    .then((obj) => {
+      console.log(`Postgres:: connected to ${obj.client.database}`);
+      obj.done();
+    })
+    .catch((err) => {
+      console.error(`Postgres:: connection err  ${err.message}`);
+    });
+
+  pgp.events.on("query", (e: any) => {
+    console.log(
+      `Postgres Debug:: ${e.query} ${e.params ? JSON.stringify(e.params) : ""}`
+    );
+  });
+
+  return db;
 };
